11b92ab19183a8be65bb56975acf01f48e0057e2
WenYuhSu <cyiken90118@gmail.com>
2020-02-19 21:46:08 -0600

    completed max sum alg for 1(c) --Ken

---
+++ code/max_sum_solution.py
@@ -0,0 +1,45 @@
+import numpy as np
+import utils, compute
+
+
+def max_sum(X, W, T):
+    
+    letter_size = 26
+    seq_len = X.shape[0]
+    l = np.zeros((seq_len, letter_size))
+    tmp = [0] * (letter_size)
+    y_ans = np.zeros(seq_len, dtype=np.int)
+
+    #  li(yi) = max_{y_i-1}{ <wyi−1, xi−1> + Tyi−1,yi + li−1(yi−1) }
+    #  i: i, j: y_{i}, k: y_{i-1}
+    for i in range(1, seq_len):
+        for j in range(letter_size):
+            for k in range(letter_size):
+                tmp[k] = np.dot(W[k], X[i-1]) + T[k, j] + l[i-1, k]
+            l[i, j] = max(tmp)
+    
+    # recovery 
+    # y∗_m = argmax_ym { <wym, xm> + lm(ym) } 
+    for i in range(letter_size):
+        tmp[i] = np.dot(W[i], X[-1]) + l[-1, i]
+
+    y_ans[-1] = np.argmax(tmp)
+    print(tmp[y_ans[-1]])
+
+
+    # y∗_i−1 = argmax_{yi−1}{ <wyi−1, xi−1> + T_{yi−1,y∗i} + li−1(yi−1) }
+    for i in range(seq_len-1, 0, -1):
+        for j in range(0, letter_size):
+            tmp[j] = np.dot(W[j], X[i-1]) + T[j, y_ans[i]] + l[i-1, j]
+        y_ans[i-1] = np.argmax(tmp)
+
+    return y_ans
+
+
+
+if __name__ == "__main__":
+    X, W, T = utils.load_decode_input()
+    
+    for i in max_sum(X, W, T):
+        print (i+1)
+    
