dc7fd359029cd71416f9bb447d3f57ad472727f7
WenYuhSu <cyiken90118@gmail.com>
2020-02-28 13:16:17 -0600

    complete robustness

--- README.md
+++ README.md
@@ -1,13 +1,75 @@
-# aml2020s_assign1
+# aml2020s_assign1 - Graphical Models
 
-#####Todo List
+### Description
+
+In this part, you will implement a conditional random field for optical character recognition (OCR), with emphasis on inference and performance test.
+
+
+### Dataset
+The original dataset is downloaded from http://www.seas.upenn.edu/∼taskar/ocr. 
+
+
+### Environments and Required Packages
+```bash
+$ python3 --version
+> version >= 3.5
+
+$ pip3 install numpy scipy
+```
+
+### Run the program
+Under the code folder
+```bash
+cd /path/to/assign/code/
+```
+
+### Assignment 1
+##### 1(c)
+
+1. brute force solution
+```bash
+$ python3 brute_force_solution.py
+
+> [16, 5, 3]
+```
+2. max sum algorithm solution
+```bash
+$ python3 max_sum_solution.py
+```
+It would write "decode-output.txt" file under the result folder
+```bash
+$ vim ../result/decode-output.txt
+```
+
+### Assignment 2
+##### 2(a)
+```bash
+$ python3 check_grad.py
+
+> Gradient Value: 1.50211935303644e-05 (10 seqs)
+```
+It would record the w and t into "gradient.txt" file under the result folder
+```bash
+$ vim ../result/graduent.txt
+``` 
+
+##### 2(b)
+```bash
+$ python3 ref_optimize.py
+
+> Word Accuracy = 0.0956673451584763, Letter Accuracy = 0.5824872127643331.
+```
+It would store the params into the "solution.txt" file and also make the prediction of the test.txt under the "prediction.txt" file
+
+
+##### Todo List
 - [ ] 1(a)
 - [ ] 1(b)
 - [X] 1(c) write brute force and dp decoder.
 
-- [ ] 2(a) check w and t gradient, and some dp.
-- [ ] 2(b) train the crf (call 2(a) func)
-- [ ] 2 save params and w, t into files
+- [X] 2(a) check w and t gradient, and some dp.
+- [X] 2(b) train the crf (call 2(a) func)
+- [X] 2 save params and w, t into files
 
 - [ ] 3(a) write the plot func(given X, test-acc, word-acc)
 - [ ] 3(a) plot svm-mc, svm-hmm, crf
---
+++ code/transform.py
@@ -0,0 +1,123 @@
+import cv2
+import numpy as np
+import utils,string
+import matplotlib.pyplot as mp
+
+from scipy import ndimage, misc
+import math
+#, compute, check_grad
+
+def read_data():
+    train_data = utils.read_data_seq('../data/train_mini.txt')
+    trainX, trainY = [], []
+    for d in train_data:
+        trainX.append(d[1])
+        trainY.append(d[0])
+
+    return trainX, trainY
+
+#offset (tx, ty)
+def translate(features, tx, ty):
+    features = features.reshape(16, 8)
+    
+    lenX, lenY = features.shape
+    translated = np.zeros((lenX, lenY), features.dtype)
+
+    M = np.float32([[1,0,tx],[0,1,ty]])
+    dst = cv2.warpAffine(features,M,(lenY,lenX))
+
+    # mp.imshow(dst, cmap='gray')
+    # mp.show()
+    return dst.flatten()
+
+
+def rotate(features, alpha):
+
+    X = features.reshape(16,8)
+
+    Y = misc.imrotate(X, 15)
+    lenx1, lenx2 = X.shape;
+    leny1, leny2 = Y.shape;
+
+    #  Trim the result back to the original size, around its center point.
+    fromx = math.floor((leny1 + 1 - lenx1)/2);
+    fromy = math.floor((leny2 + 1 - lenx2)/2);
+
+    destination = Y[fromx:fromx+lenx1, fromy:fromy+lenx2]
+
+    img_binary = cv2.threshold(destination, 128,255, cv2.THRESH_BINARY)[1]
+
+    rows,col=np.where(img_binary == 255)
+    img_binary[rows, col] = 1
+
+    return img_binary.flatten()
+
+
+def transform_data(data, n):
+    f = open('../data/transform.txt', 'r')
+    trans_list = []
+    for line in f:
+        line_list = line.strip().split(' ')
+        # print(line_list)
+        if line_list != []:
+            trans_list.append([line_list[0], int(line_list[1]), line_list[2:]])
+    f.close()
+    # print(trans_list)
+
+    for i in range(n):
+        if trans_list[i][0] == 'r':
+            _id = trans_list[i][1]
+
+            new_img = [rotate(j, int(trans_list[i][2][0])) for j in train_data[_id][1]]
+            train_data[_id][1] = new_img
+
+            # print("rotate")
+            # rotate()
+        
+        elif trans_list[i][0] == 't':
+            _id = trans_list[i][1]
+            new_img = [translate(j, int(trans_list[i][2][0]), int(trans_list[i][2][1])) for j in train_data[_id][1]]
+            train_data[_id][1] = new_img
+
+            # print("translate")
+            # translate()
+        
+        
+
+
+if __name__ == "__main__":
+    train_data = utils.read_data_seq('../data/train.txt')
+    train_data_new = transform_data(train_data, 100)
+
+    # X, Y = read_data()
+    # #train_data = utils.read_data_seq('../data/train_mini.txt') 
+
+    # #p, letter = train_data[1], train_data[0]
+    # pic = X[0][1]
+
+    # print(np.array(pic).shape)
+    
+    # pic_d = np.array(pic).reshape(16,8)
+    # print(pic_d.shape)
+
+    # #original figure
+    # #mp.figure(3)
+    # mp.imshow(pic_d, cmap = 'gray')
+    # mp.show()
+    # #translation
+    # translate_data(pic_d, 3, 3)
+
+    # #lists of transformations from translation.txt (haven't done yet)
+    # f = open('../data/translation_mini.txt', 'r')
+
+    # for line in f:
+    #     offset = []
+    #     content = line.rstrip().split(' ')
+    #     flag, nth, offx, offy = content[0], int(content[1]), int(content[2]), int(content[3])
+    #     #if(flag=='t'):
+    #         #print('word count: ', nth, '(', offx, offy, ')')
+    #         #print('origin data:\n', X[nth])
+    #         #print(np.array(X[nth]).shape)
+    #         #translate_data(pic[nth], offx, offy)
+
+
